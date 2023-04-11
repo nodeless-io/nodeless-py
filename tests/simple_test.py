f651350651e639816b0c48e7ce5312edc0a5633f
bitkarrot <73979971+bitkarrot@users.noreply.github.com>
2023-04-11 15:24:35 -0700

    clean up test file

--- tests/simple_test.py
+++ tests/simple_test.py
@@ -1,16 +1,14 @@
 import requests
-import json
 from yaml import safe_load
 
 
-def get_api_from_config(config_file):
+def get_api_from_config(config_file) -> str:
     try:
         if config_file:
             with open(config_file, "rb") as f:
                 cfile = safe_load(f)
                 api_key = cfile["apikey"]
                 return api_key
-            f.close()
     except Exception as e:
         return e
 
